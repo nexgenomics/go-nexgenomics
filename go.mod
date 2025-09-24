fb5fbc291a05f68d931795c2fe8f4ca92c3ad601
Francis Cianfrocca <garbagecat10@gmail.com>
2025-09-24 05:06:43 +0000

    Created package nexgenomics and ping function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nexgenomics/go-nexgenomics
+
+go 1.24.6
---
+++ nexgenomics.go
@@ -0,0 +1,17 @@
+package nexgenomics
+
+
+
+import (
+	"log"
+)
+
+
+/****
+Ping
+****/
+
+func Ping() error {
+	log.Printf ("nexgenomics PING")
+	return nil
+}
