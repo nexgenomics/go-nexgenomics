cb6cd09cff8eb68d76e2b46a7fc8a4d9a7a104df
Francis Cianfrocca <garbagecat10@gmail.com>
2025-09-25 07:12:53 +0000

    removed dead code

--- agentstore_test.go
+++ agentstore_test.go
@@ -15,21 +15,4 @@ func init() {
 
 func TestNewAgentstore(t *testing.T) {
 	t.Logf("agentstore token [%s]", AGENTSTORE_TOKEN)
-	/*
-	   h := nexgenomics.NewWebhook(TEST_WEBHOOK_TOKEN)
-
-	   	sentences := []string{
-	   		"This is thing 1",
-	   		"This is thing 2",
-	   		"This is thing 3",
-	   		"This is thing 4",
-	   		"This is thing 5",
-	   	}
-
-	   e := h.SendSentences(sentences...)
-
-	   	if e != nil {
-	   		t.Errorf("%s", e)
-	   	}
-	*/
 }
