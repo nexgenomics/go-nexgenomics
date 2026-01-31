5708832ec127cf00dfc2c9d5dbde050e866b9f92
Francis Cianfrocca <cianfrocca@insightcyber.com>
2026-01-31 15:03:33 +0000

    dev

--- fabric/points.go
+++ fabric/points.go
@@ -1,21 +1,51 @@
 package fabric
 
+import (
+	"context"
+	"fmt"
+	"log"
+)
+
 
 type ID string
 
 // Point
 type Point struct {
-	Id: ID
-	Embedding: []float32
-	Content: []byte
-	Metadata: map[string]string
+	Id ID
+	Embedding []float32
+	Content []byte
+	Metadata map[string]string
 	// scores, etc
 }
 
 
 // UpsertPoint
-func UpsertPoint(pt *Point) (ID,error) {
+func UpsertPoint(ctx context.Context, model string, pt *Point) (ID,error) {
 
+	if model == "" {
+		model = "vernon-002"
+	}
 
+	resp,e := Call(&CallCfg{
+		Ctx:      ctx,
+		Tenant:   "0",
+		Agent:    model,
+		Method: "put",
+		Endpoint: "point",
+		Body: pt,
+	})
+
+	if e != nil {
+		return "", e
+	}
+
+	t, ok := resp.Body.(map[string]any)
+	if !ok {
+		return "", fmt.Errorf("No reply from model")
+	}
+
+	log.Printf("VERNON %v", t)
+
+	return "NOTHING",nil
 }
 
